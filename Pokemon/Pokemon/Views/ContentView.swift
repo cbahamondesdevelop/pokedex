a4d5b7b409ab5445e1eb428672604e1cc913e6a3
cbahamondesdevelop <106202708+cbahamondesdevelop@users.noreply.github.com>
2022-08-30 21:01:11 -0400

    Add files via upload
    APP para listar y ver pokemones

---
+++ Pokemon/Pokemon/Views/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Pokemon
+//
+//  Created by Cristian Bahamondes on 25-08-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var vm = ViewModel()
+    
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 10) {
+                    ForEach(vm.filteredPokemon) { pokemon in
+                         NavigationLink(destination: PokemonDetailView(pokemon: pokemon))
+                        {
+                            PokemonView(pokemon: pokemon)
+                        }
+                    }
+                }
+                .animation(.easeIn(duration: 0.3), value: vm.filteredPokemon.count)
+                .navigationTitle("PokeDEX")
+            }
+            .searchable(text: $vm.searchText)
+        }
+        .environmentObject(vm)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
